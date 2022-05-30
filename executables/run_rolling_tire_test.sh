61c5794f838f3734ff82a2eb88488be055f15353
Quercus <63294776+quercushernandez@users.noreply.github.com>
2022-05-30 16:21:05 +0200

    Update run_rolling_tire_test.sh

--- executables/run_rolling_tire_test.sh
+++ executables/run_rolling_tire_test.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 python main.py --sys_name rolling_tire --train_SAE False --train_SPNN False \
     --layer_vec_SAE_q 12420, 40, 40, 10 --layer_vec_SAE_v 12420, 40, 40, 10 --layer_vec_SAE_sigma 24840, 80, 80, 20 \
     --hidden_vec_SPNN 198, 198, 198, 198, 198 \
